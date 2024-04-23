76a477516f3f710123063f2c2775034e1baa0c8e
Dheeraj868 <adithyasingarao1234@gmail.com>
2024-04-23 19:54:02 +0530

    my commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let input = document.getElementById("inp");
+let text = document.querySelector(".text");
+
+function Add(){
+if(input.value === ''){
+        alert("You must write something!");
+}
+else{
+        let newEle = document.createElement("ul");
+        newEle.innerHTML = `${input.value} <i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newEle);
+        input.value = '';
+        newEle.querySelector("i").addEventListener("click" , remove);
+        function remove(){
+                newEle.remove();
+        }
+}
+
+}
